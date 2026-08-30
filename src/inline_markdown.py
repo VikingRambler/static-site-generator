6cc66e39c20d8c7d37f4d50ba84fbb08f83b5fff
cyberviking <dan@dccyber.co.uk>
2026-08-30 17:39:06 +0100

    Implemented logic for Markdown Images and Links

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -29,3 +29,42 @@ def extract_markdown_images(text: str) -> list[tuple]:
 def extract_markdown_links(text) -> list[tuple]:
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
+
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        images = extract_markdown_images(node.text)
+        split_nodes = []
+        original_text = node.text
+        if len(images) == 0:
+            new_nodes.append(node)
+        for i in range(len(images)):
+            sections = original_text.split(f"![{images[i][0]}]({images[i][1]})", 1)
+            if sections[0] != "":
+                split_nodes.append(TextNode(sections[0], TextType.TEXT))
+                split_nodes.append(TextNode(images[i][0], TextType.IMAGE, images[i][1]))
+            original_text = sections[1]
+        if original_text != "":
+            split_nodes.append(TextNode(original_text, TextType.TEXT))
+        new_nodes.extend(split_nodes)
+    return new_nodes
+
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        links = extract_markdown_links(node.text)
+        split_nodes = []
+        sections = []
+        original_text = node.text
+        if len(links) == 0:
+            new_nodes.append(node)
+        for i in range(len(links)):
+            sections = original_text.split(f"[{links[i][0]}]({links[i][1]})", 1)
+            if sections[0] != "":
+                split_nodes.append(TextNode(sections[0], TextType.TEXT))
+                split_nodes.append(TextNode(links[i][0], TextType.LINK, links[i][1]))
+            original_text = sections[1]
+        if original_text != "":
+            split_nodes.append(TextNode(original_text, TextType.TEXT))
+        new_nodes.extend(split_nodes)
+    return new_nodes
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
 from textnode import TextNode, TextType
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -109,6 +109,38 @@ class TestInlineMarkdown(unittest.TestCase):
             "This is text with a link to [DCCyber](https://dccyber.co.uk) and a link to [python.dccyber](https://python.dccyber.co.uk)"
         )
         self.assertListEqual([("DCCyber", "https://dccyber.co.uk"), ("python.dccyber", "https://python.dccyber.co.uk")], matches)
+
+    def test_split_images(self):
+        node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        TextType.TEXT,
+    )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+        [
+            TextNode("This is text with an ", TextType.TEXT),
+            TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and another ", TextType.TEXT),
+            TextNode("second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"),
+        ],
+        new_nodes,
+    )
+
+    def test_split_links(self):
+        node = TextNode(
+        "This is text with a link to [DCCYBER](https://dccyber.co.uk) and another to [PYTHON.DCCYBER](https://python.dccyber.co.uk)",
+        TextType.TEXT,
+    )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+        [
+            TextNode("This is text with a link to ", TextType.TEXT),
+            TextNode("DCCYBER", TextType.LINK, "https://dccyber.co.uk"),
+            TextNode(" and another to ", TextType.TEXT),
+            TextNode("PYTHON.DCCYBER", TextType.LINK, "https://python.dccyber.co.uk"),
+        ],
+        new_nodes,
+    )
 
 if __name__ == '__main__':
      unittest.main()
